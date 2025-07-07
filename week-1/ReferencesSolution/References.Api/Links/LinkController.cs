8bc16d966e4a018dfc7f6ee104118f7ce14e85da
Samuel Shiau <shiausamuel@gmail.com>
2025-07-07 19:45:21 +0000

    ss

---
+++ week-1/ReferencesSolution/References.Api/Links/LinkController.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace References.Api.Links;
+public class LinkController : ControllerBase
+{
+    //some code here that will get called when a get /links is sent to the server
+
+    [HttpGet("/links")]
+    public async Task<ActionResult> GetAllLinksAsync(CancellationToken token) {
+        return Ok();
+    }
+
+}
