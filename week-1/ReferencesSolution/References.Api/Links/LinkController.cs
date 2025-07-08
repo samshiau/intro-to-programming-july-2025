a625168c3e97085184d5d6980b3bdabb545a00ec
Samuel Shiau <shiausamuel@gmail.com>
2025-07-08 20:38:26 +0000

    backend with db

--- week-1/ReferencesSolution/References.Api/Links/LinkController.cs
+++ week-1/ReferencesSolution/References.Api/Links/LinkController.cs
@@ -1,14 +1,51 @@
 ﻿using System.Diagnostics.Contracts;
+using Marten;
 using Microsoft.AspNetCore.Mvc;
 
-namespace References.Api.Links;
-public class LinkController : ControllerBase
+public class LinkController(IDocumentSession session) : ControllerBase
 {
-    //some code here that will get called when a get /links is sent to the server
+
+
+    // some code here that will get called when a GET /links is sent to this server.
 
     [HttpGet("/links")]
-    public async Task<ActionResult> GetAllLinksAsync(CancellationToken token) {
-        return Ok();
+    public async Task<ActionResult> GetAllLinksAsync(CancellationToken token)
+    {
+        var response = await session.Query<LinkEntity>().ToListAsync();
+        return Ok(response);
     }
 
+    [HttpPost("/links")]
+    public async Task<ActionResult> AddALinkAsync([FromBody] LinkCreateRequest request, CancellationToken token)
+    {
+
+        // do your validation, did they send the right thing.
+        // create an entity and save that to the database
+        var entityToSave = new LinkEntity()
+        {
+            Id = Guid.NewGuid(),
+            Href = request.Href,
+            Description = request.Description,
+        };
+        session.Store(entityToSave);
+        await session.SaveChangesAsync();
+        // return them a "copy" of what they saved to the database.
+        return Ok(entityToSave);
+    }
+
+
+}
+
+/*{
+    "href": "https://microsoft.com",
+    "description": "Microsoft's Main Site"
+}*/
+
+public record LinkCreateRequest(string Href, string Description);
+
+public class LinkEntity
+{
+    public Guid Id { get; set; }
+    public string Href { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
 }
--- week-1/ReferencesSolution/References.Api/Program.cs
+++ week-1/ReferencesSolution/References.Api/Program.cs
@@ -1,33 +1,39 @@
 
-namespace References.Api
+
+
+
+using Marten;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("links") ?? throw new Exception("No connection string");  ;
+
+Console.WriteLine(connectionString);
+
+// Add services to the container. 
+builder.Services.AddMarten(config => { config.Connection(connectionString); }).UseLightweightSessions();
+
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
-            }
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();  // scan through all the controllers and map the endpoint to the methods. Setups routings
-
-
-            app.Run();
-        }
-    }
+    app.MapOpenApi();
 }
+
+app.UseAuthorization();
+
+
+app.MapControllers();  // Reflection the ability to have code that looks at itself.
+
+app.Run();
+
+
+// I will explain this in detail later and you will be bored as heck.
+
+public partial class Program;
