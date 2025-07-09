6e2ec5f2058c6c0e5bdae069c5dcf9df5c9a67f3
Samuel Shiau <shiausamuel@gmail.com>
2025-07-09 18:28:29 +0000

    7925

---
+++ week-1/BankingSolution/Banking.Domain/BankAccount.cs
@@ -0,0 +1,24 @@
+﻿
+
+
+namespace Banking.Domain
+{
+    public class BankAccount
+    {
+        private decimal _currentBalance = 5000;
+        public void Deposit(decimal amountToDeposit)
+        {
+            _currentBalance = _currentBalance + amountToDeposit;
+        }
+
+        public decimal GetBalance()
+        {
+            return _currentBalance;
+        }
+
+        public void Withdraw(decimal amountToWithdraw)
+        {
+            _currentBalance -= amountToWithdraw;
+        }
+    }
+}
---
+++ week-1/BankingSolution/Banking.Tests/Account/MakingDeposaits.cs
@@ -0,0 +1,24 @@
+﻿
+
+using System.Runtime.InteropServices;
+using Banking.Domain;
+
+namespace Banking.Tests.Account;
+
+public class MakingDeposaits
+{
+    [Theory]
+    [InlineData(100)]
+    [InlineData(223.89)]
+    [InlineData(0)]
+    [InlineData(-1002.38)]
+    public void MakingADepositIncreasesTheBalance(decimal amountToDeposit) 
+    {
+        var account = new BankAccount();
+        var openingBalance = account.GetBalance();
+
+        account.Deposit(amountToDeposit);
+
+        Assert.Equal(amountToDeposit+openingBalance, account.GetBalance());
+    }
+}
